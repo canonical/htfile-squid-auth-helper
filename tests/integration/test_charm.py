1fa7f7915fc22a8a2a3071433acbbeb3df76d516
Sebastien Georget <seb4stien@georget.name>
2024-11-26 12:53:35 +0100

    Break the monolithic test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -7,27 +7,44 @@
 
 import asyncio
 import logging
+import uuid
 from pathlib import Path
 
 import pytest
 import requests
 import yaml
 from pytest_operator.plugin import OpsTest
+from requests.auth import HTTPBasicAuth, HTTPDigestAuth
 
 logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text(encoding="utf-8"))
 APP_NAME = METADATA["name"]
-PRINCIPAL_CHARM = "squid-reverseproxy"
-WEBSITE_CHARM = "apache2"
+SQUID_CHARM = "squid-reverseproxy"
+APACHE_CHARM = "apache2"
+
+
+def squid_url(ops_test: OpsTest) -> str:
+    """Retrieve test URL from the SQUID first unit.
+
+    Args:
+        ops_test: the current model.
+
+    Returns:
+        The URL to access the Squid proxy.
+    """
+    ip = ops_test.model.applications[SQUID_CHARM].units[0].public_address
+    # IPv6
+    if ":" in ip:
+        ip = f"[{ip}]"
+
+    return f"http://{ip}:3128"
 
 
 @pytest.mark.abort_on_fail
+@pytest.mark.skip_if_deployed
 async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
-    """Deploy the charm together with related charms.
-
-    Assert on the unit status before any relations/configurations take place.
-    """
+    """Deploy the charm alone."""
     # Deploy the charm and wait for active/idle status
     charm = pytestconfig.getoption("--charm-file")
     if not charm:
@@ -35,7 +52,6 @@ async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
 
     assert ops_test.model
 
-    logger.info("Deploying subordinate charm alone")
     await asyncio.gather(
         ops_test.model.deploy(
             f"./{charm}", application_name=APP_NAME, num_units=0, series="jammy"
@@ -45,43 +61,34 @@ async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
         ),
     )
 
-    logger.info("Deploying %s and %s", PRINCIPAL_CHARM, WEBSITE_CHARM)
+
+@pytest.mark.skip_if_deployed
+async def test_no_subordinate(ops_test: OpsTest, pytestconfig: pytest.Config):
+    """Check testing environment. Deploy Squid proxy and Apache backend.
+    Set proxy relation and check access without subordinate integration.
+    """
     await asyncio.gather(
-        ops_test.model.deploy(PRINCIPAL_CHARM, application_name=PRINCIPAL_CHARM, series="jammy"),
-        ops_test.model.deploy(WEBSITE_CHARM, application_name=WEBSITE_CHARM),
-        ops_test.model.wait_for_idle(apps=[WEBSITE_CHARM], status="active", raise_on_blocked=True),
-        ops_test.model.wait_for_idle(
-            apps=[PRINCIPAL_CHARM], status="unknown", raise_on_blocked=True
-        ),
+        ops_test.model.deploy(SQUID_CHARM, application_name=SQUID_CHARM, series="jammy"),
+        ops_test.model.deploy(APACHE_CHARM, application_name=APACHE_CHARM),
+        ops_test.model.wait_for_idle(apps=[APACHE_CHARM], status="active", raise_on_blocked=True),
+        ops_test.model.wait_for_idle(apps=[SQUID_CHARM], status="unknown", raise_on_blocked=True),
     )
 
-    logger.info("Validating initial setup")
-    ip = ops_test.model.applications[PRINCIPAL_CHARM].units[0].public_address
-
-    # IPv6
-    if ":" in ip:
-        ip = f"[{ip}]"
-
-    url = f"http://{ip}:3128"
-    assert requests.get(url, timeout=5).status_code == 403
-
-    logger.info("Setting up %s as a proxy to %s", PRINCIPAL_CHARM, WEBSITE_CHARM)
     await asyncio.gather(
-        ops_test.model.relate(f"{PRINCIPAL_CHARM}:website", f"{WEBSITE_CHARM}:website"),
-        ops_test.model.wait_for_idle(apps=[WEBSITE_CHARM], status="active", raise_on_blocked=True),
-        ops_test.model.wait_for_idle(
-            apps=[PRINCIPAL_CHARM], status="unknown", raise_on_blocked=True
-        ),
+        ops_test.model.relate(f"{SQUID_CHARM}:website", f"{APACHE_CHARM}:website"),
+        ops_test.model.wait_for_idle(apps=[APACHE_CHARM], status="active", raise_on_blocked=True),
+        ops_test.model.wait_for_idle(apps=[SQUID_CHARM], status="unknown", raise_on_blocked=True),
     )
 
-    logger.info("Validating proxy setup")
-    assert requests.get(url, timeout=5).status_code == 200
+    assert requests.get(squid_url(ops_test), timeout=5).status_code == 200
 
-    logger.info("Enable local auth through our subordinate")
+
+async def test_subordinate_relation(ops_test: OpsTest, pytestconfig: pytest.Config):
+    """Relate our charm to Squid and ensure anonymous access is still ok."""
     await asyncio.gather(
-        ops_test.model.relate(APP_NAME, PRINCIPAL_CHARM),
+        ops_test.model.relate(APP_NAME, SQUID_CHARM),
         ops_test.model.wait_for_idle(
-            apps=[PRINCIPAL_CHARM],
+            apps=[SQUID_CHARM],
             status="unknown",
         ),
         ops_test.model.wait_for_idle(
@@ -90,13 +97,75 @@ async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
         ),
     )
 
-    logger.info("Setting up ACL and validating it")
+    assert requests.get(squid_url(ops_test), timeout=5).status_code == 200
+
+
+async def test_required_auth(ops_test: OpsTest, pytestconfig: pytest.Config):
+    """Define an ACL requiring authentication and ensure anonymous users are blocked."""
     await asyncio.gather(
-        ops_test.model.applications[PRINCIPAL_CHARM].set_config(
+        ops_test.model.applications[SQUID_CHARM].set_config(
             {"auth_list": '- "!proxy_auth": [REQUIRED]\n  http_access: deny all'}
         ),
-        ops_test.model.wait_for_idle(
-            apps=[PRINCIPAL_CHARM], status="unknown", raise_on_blocked=True
-        ),
+        ops_test.model.wait_for_idle(apps=[SQUID_CHARM], status="unknown", raise_on_blocked=True),
     )
-    assert requests.get(url, timeout=5).status_code == 401
+    assert requests.get(squid_url(ops_test), timeout=5).status_code == 401
+
+
+async def test_digest_auth(ops_test: OpsTest, pytestconfig: pytest.Config):
+    """Config Digest auth.
+    Check that authenticated users have access, and that non authenticated don't.
+    """
+    await asyncio.gather(
+        ops_test.model.applications[APP_NAME].set_config({"authentication-type": "digest"}),
+        ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", raise_on_blocked=True),
+    )
+
+    user = str(uuid.uuid4())
+    action = (
+        await ops_test.model.applications[APP_NAME]
+        .units[0]
+        .run_action("create-user", username=user)
+    )
+    await action.wait()
+    assert action.status == "completed"
+
+    password = action.results["password"]
+    assert password
+
+    response = requests.get(squid_url(ops_test), auth=HTTPDigestAuth(user, password), timeout=5)
+    assert response.status_code == 200
+
+    response = requests.get(
+        squid_url(ops_test), auth=HTTPDigestAuth(user, "badpassword"), timeout=5
+    )
+    assert response.status_code == 401
+
+
+async def test_basic_auth(ops_test: OpsTest, pytestconfig: pytest.Config):
+    """Config Basic auth.
+    Check that authenticated users have access, and that non authenticated don't.
+    """
+    await asyncio.gather(
+        ops_test.model.applications[APP_NAME].set_config({"authentication-type": "basic"}),
+        ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", raise_on_blocked=True),
+    )
+
+    user = str(uuid.uuid4())
+    action = (
+        await ops_test.model.applications[APP_NAME]
+        .units[0]
+        .run_action("create-user", username=user)
+    )
+    await action.wait()
+    assert action.status == "completed"
+
+    password = action.results["password"]
+    assert password
+
+    response = requests.get(squid_url(ops_test), auth=HTTPBasicAuth(user, password), timeout=5)
+    assert response.status_code == 200
+
+    response = requests.get(
+        squid_url(ops_test), auth=HTTPBasicAuth(user, "badpassword"), timeout=5
+    )
+    assert response.status_code == 401
